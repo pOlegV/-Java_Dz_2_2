cccf0ee09c217ae70f1a98039ee1bd68a08f5790
pOlegV <oleg-1@yandex.ru>
2022-07-07 11:08:27 +0300

    Raschet bonusov i balansa

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,17 @@ public class Main {
         // сумма пополнения порог и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+        int cshet = 100;
+        int summaPopolneniy = 1100;
+        int bonus;
+        int itog;
+        if (summaPopolneniy <= 100) {
+            itog = summaPopolneniy + cshet;
+            System.out.println("Клиент пополнил счёт на " + summaPopolneniy + " рублей - бонусов нет, итоговая сумма на счету клиента : " + itog + " рублей");
+        } else {
+            bonus = summaPopolneniy / 100;
+            itog = bonus + summaPopolneniy + cshet;
+            System.out.println("Клиент пополнил счёт на " + summaPopolneniy + " рублей - бонусов " + bonus + " , итоговая сумма на счету клиента : " + itog + " рублей");
+        }
     }
 }
